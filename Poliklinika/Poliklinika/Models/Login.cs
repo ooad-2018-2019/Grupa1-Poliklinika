3de4e84c216b7889151952ce393d97ba8b47d9a4
epita1 <epita1@etf.unsa.ba>
2019-06-18 16:43:28 +0200

    Popravljena Login klasa.

--- Poliklinika/Poliklinika/Models/Login.cs
+++ Poliklinika/Poliklinika/Models/Login.cs
@@ -7,19 +7,19 @@ namespace Poliklinika.Models
 {
     public class Login
     {
-     public Osoba osoba { get; set; }
-       public String username { get; set; }
-       public String password { get; set; }
+        private Osoba _osoba;
+        private String _username;
+        private String _password;
 
         public Login(Osoba osoba, string username, string password)
         {
-            this.osoba = osoba;
-            this.username = username;
-            this.password = password;
+            this.Osoba = osoba;
+            this.Username = username;
+            this.Password = password;
         }
 
-
-
-
+        public Osoba Osoba { get => _osoba; set => _osoba = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
     }
 }
