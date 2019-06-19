7b779b8c2c0831b140f73428c81e639689cc5ad6
epita1 <epita1@etf.unsa.ba>
2019-06-19 16:21:55 +0200

    Kreiran PacijentController.

---
+++ Poliklinika/Poliklinika/Controllers/PacijentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTermin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Poliklinika.Controllers
+{
+    public class PacijentController : Controller
+    {
+        private Administrator administrator = Administrator.Instance();
+        public static DateTime unosDatuma;
+
+
+        
+        public PacijentController()
+        {
+        }
+    }
+}
